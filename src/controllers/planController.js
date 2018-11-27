c8cfb022f05114f6eb04dc371d46ed4446b0f583
basedHokage <eugene.omar@andela.com>
2018-11-27 18:51:58 +0300

    add controllers and controller methods

--- src/controllers/planController.js
+++ src/controllers/planController.js
@@ -1,5 +1,54 @@
-module.export = {
-  addPlan: (req, res) => {
-    
+const { Member, Plan } = require('../models')
+
+const addPlan = async (req, res) => {
+  const [created, newPlan] = await Plan.findOrCreate({
+    where: req.body,
+    defaults: req.body,
+    plain: true
+  })
+  if (created) {
+    res.status(201).json(newPlan)
+    return
   }
+  res.status(409).json({
+    message: 'That plan already exists'
+  })
+};
+
+const updatePlan = async (req, res) => {
+  const plan = await Plan.findById(req.params.planId)
+  await plan.update(req.body)
+  res.status(200).json(plan)
+};
+
+const deletePlan = async (req, res) => {
+  const plan = await Plan.findById(req.params.planId);
+  await plan.delete();
+  res.status(200).json({
+    message: 'That plan was deleted'
+  });
+};
+
+const listMembers = async (req, res) => {
+  const plan = await Plan.findById(req.params.planId);
+  const members = plan.members
+  res.status(200).json(members);
+};
+
+const listMembers = async (req, res) => {
+  const member = await Member.findById(req.params.memberId);
+  await member.update({ planId: null });
+  res.status(200).json({
+    message: 'Member successfully removed from plan'
+  });
+};
+
+const addMember = async (req, res) => {
+  const member = await Member.findById(req.params.memberId)
+  await member.update({ planId: req.params.planId })
+  res.status(200).json({
+    message: 'Member successfully added'
+  })
 }
+
+module.exports = { addPlan, updatePlan, deletePlan, listMembers, addMember }
---
+++ src/controllers/userController.js
@@ -0,0 +1,32 @@
+const { Members, Plans } = require('../models');
+
+const addUser = async (req, res) => {
+  const [created, newUser] = await Members.findOrCreate({
+    where: req.body,
+    defaults: req.body,
+    plain: true
+  });
+  if (created) {
+    res.status(201).json(newUser);
+    return;
+  }
+  res.status(409).json({
+    message: 'That user already exists'
+  });
+};
+
+const updateUser = async (req, res) => {
+  const user = await Members.findById(req.params.userId);
+  await user.update(req.body);
+  res.status(200).json(user);
+};
+
+const deleteUser = async (req, res) => {
+  const user = await Members.findById(req.params.userId);
+  await user.delete();
+  res.status(200).json({
+    message: 'That user has been deleted'
+  });
+};
+
+module.exports = { addUser, updateUser, deleteUser };
